53a615eb5c02701bbc6c8e39ae45fbc3e2f0192d
Earle Skinner <earle.skinner@gmail.com>
2017-09-24 02:15:51 +0100

    Added time taken tests for the prime number generator

--- src/PrimeTables.Tests/PrimeNumberTests.cs
+++ src/PrimeTables.Tests/PrimeNumberTests.cs
@@ -2,6 +2,7 @@
 
 namespace PrimeTables.Tests
 {
+    using System;
     using System.Collections.Generic;
     using NUnit.Framework;
 
@@ -20,12 +21,41 @@ namespace PrimeTables.Tests
             }
         }
 
+        /// <summary>
+        /// Test the amount of time the generator takes (in milliseconds)
+        /// </summary>
+        /// <remarks>
+        /// The timeouts should be adjusted as code is optimised
+        /// </remarks>
+        private static IEnumerable<TestCaseData> Timeouts
+        {
+            get
+            {
+                yield return new TestCaseData(new PrimeNumberGenerator(0), 1);
+                yield return new TestCaseData(new PrimeNumberGenerator(1), 1);
+                yield return new TestCaseData(new PrimeNumberGenerator(3), 1);
+                yield return new TestCaseData(new PrimeNumberGenerator(10000), 500);
+                yield return new TestCaseData(new PrimeNumberGenerator(20000), 1000);
+            }
+        }
+
         [Test]
         [TestCaseSource(nameof(ExpectedPrimeNumbers))]
         public void Should_match_expected_prime_numbers(PrimeNumberGenerator generator, long[] expectedPrimeNumbers)
         {
             var actualPrimeNumbers = generator.GeneratePrimeNumbers();
             Assert.AreEqual(actualPrimeNumbers, expectedPrimeNumbers);
+        }
+
+        [Test]
+        [TestCaseSource(nameof(Timeouts))]
+        public void Should_not_timeout_when_generating_prime_numbers(PrimeNumberGenerator generator, int maximumTime)
+        {
+            var start = DateTime.Now;
+            generator.GeneratePrimeNumbers();
+            var timeTaken = (DateTime.Now - start).Milliseconds;
+            Console.WriteLine($"Time taken: {timeTaken}ms to generate {generator.UpperBound} prime numbers");
+            Assert.LessOrEqual(timeTaken, maximumTime);
         }
     }
 }
